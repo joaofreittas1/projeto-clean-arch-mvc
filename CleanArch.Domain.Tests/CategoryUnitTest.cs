a1c83ba9d8162770644d02cf64fb84e6d178f46c
Joao Freittas <joaofreittas461@gmail.com>
2026-05-16 12:35:34 -0300

    feat: criando projeto de testes

---
+++ CleanArch.Domain.Tests/CategoryUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Tests;
+
+public class CategoryUnitTest
+{
+    [Fact]
+    public void Create_Category_WithValidParameters_ResultObjectValidState()
+    {
+        
+    }
+}
